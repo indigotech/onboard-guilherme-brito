150ecdd6c5580ce2e281436b52bb0b83b4b962a4
Guilherme Brito <guilherme.brito@taqtile.com.br>
2024-09-04 10:43:03 -0300

    feature: implement create user mutation

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,44 @@ const typeDefs = `#graphql
   type Query {
     hello: String
   }
+
+  type Mutation {
+    createUser(data: UserInput!): User!
+  }
+
+  input UserInput {
+    name: String!
+    email: String!
+    password: String!
+    birthDate: String!
+  }
+
+  type User {
+    id: Int!
+    name: String!
+    email: String!
+    birthDate: String!
+  }
 `;
+
+interface UserInput {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+}
 
 const resolvers = {
   Query: {
     hello: () => 'Hello World',
+  },
+  Mutation: {
+    createUser: (_, args: { data: UserInput }) => {
+      return {
+        id: 1,
+        ...args.data,
+      };
+    },
   },
 };
 
