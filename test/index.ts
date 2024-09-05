da07bc8982640c7a62e544e02fba09ca923c3e32
Guilherme Brito <guilherme.brito@taqtile.com.br>
2024-09-05 11:41:31 -0300

    feature: add test for hello query and automation to start and stop the server

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,31 @@
-import { describe, it } from 'mocha';
+import { describe, it, before, after } from 'mocha';
+import { startServer, server } from '../src/server.js';
+import axios from 'axios';
+
+const LOCAL_SERVER_URL = 'http://localhost:4000';
 
 describe('Onboard Guilherme Brito', () => {
   describe('#core', () => {
     it('should print something in terminal', () => console.log('Core test working!'));
+  });
+
+  describe('#e2e apis', () => {
+    before(async () => {
+      await startServer();
+    });
+
+    after(() => {
+      server.stop();
+    });
+
+    it('should receive Hello World from hello query', async () => {
+      const graphqlQuery = {
+        operationName: 'Hello',
+        query: 'query Hello { hello }',
+        variables: {},
+      };
+      const res = await axios.post(LOCAL_SERVER_URL, graphqlQuery);
+      console.log(res.data);
+    });
   });
 });
