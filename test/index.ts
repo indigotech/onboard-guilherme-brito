5e2cf61e8c70c50492bf0928471c20d3dba09325
Guilherme Brito <guilherme.brito@taqtile.com.br>
2024-09-05 09:15:30 -0300

    feature: add test basic setup

---
+++ test/index.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'mocha';
+
+describe('Onboard Guilherme Brito', () => {
+  describe('#core', () => {
+    it('should print something in terminal', () => console.log('Core test working!'));
+  });
+});
