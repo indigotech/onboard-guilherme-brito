687d2415a575e919fc84570b1ea02af1d8317cfc
Guilherme Brito <guilherme.brito@taqtile.com.br>
2024-09-10 10:16:04 -0300

    feature: implement get single user query

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,10 +1,12 @@
 import { createUserResolver } from './create-user-resolver.js';
 import { helloResolver } from './hello-resolver.js';
 import { loginResolver } from './login-resolver.js';
+import { userResolver } from './query-user-resolver.js';
 
 export const resolvers = {
   Query: {
     hello: helloResolver,
+    user: userResolver,
   },
   Mutation: {
     createUser: createUserResolver,
